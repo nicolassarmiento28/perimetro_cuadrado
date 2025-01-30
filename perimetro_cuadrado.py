15bb1cbfadd858c31a255ca6b3a9e2fec3ca9526
nicolassarmiento28 <109758852+nicolassarmiento28@users.noreply.github.com>
2025-01-29 22:47:00 -0300

    Create perimetro_cuadrado.py

---
+++ perimetro_cuadrado.py
@@ -0,0 +1,12 @@
+# CALCULAR EL PERIMETRO DE UN CUADRADO
+
+# 1 - SOLICITAR LA LONGITUD DEL LADO
+longitud_lado = float(input("Introduzca la longitud de un Lado del cuadrado en cm :"))
+print (f"la longitud del ladc proporcionado es : {longitud_lado}")
+
+# 2 - CALCULAR EL PERIMTERO
+perimetro_cuadrado = longitud_lado + longitud_lado + longitud_lado + longitud_lado
+
+
+# 3- MOSTRAR EL RESULTADO
+print(f"el resultado del perimetro del cuadrado en cm es : {perimetro_cuadrado}")
